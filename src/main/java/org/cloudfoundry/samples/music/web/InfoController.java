514b33120d89abf8d8e2538798cc6fb459c3c497
swagataroy <sroy@pivotal.io>
2020-05-12 00:44:49 -0400

    adding mychanges

--- src/main/java/org/cloudfoundry/samples/music/web/InfoController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/InfoController.java
@@ -5,11 +5,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.Cloud;
 import org.springframework.cloud.service.ServiceInfo;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import java.io.PrintWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+
+//import org.springframework.http.*;
 
 @RestController
 public class InfoController {
@@ -37,6 +49,7 @@ public class InfoController {
         }
     }
 
+
     private String[] getServiceNames() {
         if (cloud != null) {
             final List<ServiceInfo> serviceInfos = cloud.getServiceInfos();
@@ -50,4 +63,38 @@ public class InfoController {
             return new String[]{};
         }
     }
+
+    @RequestMapping(value = "/swagata", method = RequestMethod.GET)
+    public void redirectToTwitter(HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.sendRedirect("https://twitter.com");
+    }
+
+    @RequestMapping (value = "/swagatatest", method = RequestMethod.GET)
+    public void redirectToExternalUrl(HttpServletResponse httpServletResponse) throws  IOException, URISyntaxException {
+        URI uri = new URI("https://www.google.com");
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+        HttpStatus statusCode = response.getStatusCode();
+        System.out.println("statusCode  is "+statusCode);
+        PrintWriter out = httpServletResponse.getWriter();
+        out.println("Testing again with https");
+        out.println("statusCode  is "+statusCode);
+    }
+
+
+    @RequestMapping (value = "/swagatatesttrust", method = RequestMethod.GET)
+    public void testTrusts(HttpServletResponse httpServletResponse) throws  IOException, URISyntaxException {
+        //URI uri = new URI("https://www.google.com");
+        //URI uri = new URI("https://httpbin.org/user-agent");
+        URI uri = new URI("https://self-signed.badssl.com");
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+        HttpStatus statusCode = response.getStatusCode();
+        System.out.println("statusCode  is "+statusCode);
+        PrintWriter out = httpServletResponse.getWriter();
+        out.println("Testing again with https");
+        out.println("StatusCode  is "+statusCode);
+        out.println("Response Body is "+response.getBody());
+    }
+
 }
